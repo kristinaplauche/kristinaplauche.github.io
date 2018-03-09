ffe62178bdf6ba995f5f5ddc9f9f2ab410227c83
Kristina <kristinaplauche@Kristinas-MacBook-Pro.local>
2018-03-09 18:42:40 -0500

    Week 9 new home page
    Created home page layout.
    Added json to home page & franklin page

---
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -0,0 +1,26 @@
+var requestURL = 'http://api.wunderground.com/api/cdf3f3537bf94c1c/conditions/q/MN/Franklin.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL, true);
+request.send();
+request.onload = function () {
+    var franklinWeather = JSON.parse(request.responseText);
+    document.getElementById('desc').innerHTML = franklinWeather.current_observation.weather;
+    document.getElementById('currentTemp').innerHTML = franklinWeather.current_observation.temp_f;
+    document.getElementById('windBox').innerHTML = franklinWeather.current_observation.wind_mph;
+    document.getElementById('windchill').innerHTML = franklinWeather.current_observation.windchill_f;
+    parseFloat(document.getElementById('imageIcon').innerHTML = "<img src=\"" + franklinWeather.current_observation.icon_url + "\">");
+
+}
+
+var forecast = new XMLHttpRequest();
+forecast.open('GET', 'http://api.wunderground.com/api/cdf3f3537bf94c1c/forecast/q/MN/Franklin.json', true);
+forecast.send();
+forecast.onload = function () {
+    var franklinForecast = JSON.parse(forecast.responseText);
+    console.log(franklinForecast);
+    document.getElementById('forecast').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["0"].fcttext;
+
+
+}
+
+
